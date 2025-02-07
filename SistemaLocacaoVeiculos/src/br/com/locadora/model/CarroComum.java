b827f791148b8e86be9a766c9fab0024bf932b84
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2025-02-07 18:27:49 -0300

    Revert "Merge pull request #10 from igoreloidiasbranco/feature/Locacao/Veiculos"
    This reverts commit 8c810ae1e1e5000442d5a6024815866e62653281, reversing
    changes made to f8390ad646549c6c9bcf272a88f87b03f9f97b92.

--- SistemaLocacaoVeiculos/src/br/com/locadora/model/CarroComum.java
+++
@@ -1,7 +0,0 @@
-package br.com.locadora.model;
-
-public class CarroComum extends br.com.locadora.model.Veiculo {
-    public CarroComum(String modelo, String placa, double valorDiaria, boolean disponibilidade) {
-        super(modelo, placa, valorDiaria, disponibilidade);
-    }
-}
